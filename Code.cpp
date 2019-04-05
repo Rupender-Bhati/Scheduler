6455d71ab62efcb79367bb88fa423fff501b1213
Rupender-Bhati <bhati730@gmail.com>
2019-04-05 22:32:23 +0530

    added LOC for Class

--- Code.cpp
+++ Code.cpp
@@ -1,5 +1,31 @@
-#include<iostream.h>
+#include<iostream>
+#include<string.h>
+using namespace::std;
 
+class Process
+{
+    public int current_burst_time=0;
+    public int time_waited;
+	public int arrival_time=0;
+	int burst_time=0;
+	public String process_id;
 
+	public Process()
+	{
 
+        Cout<<"Enter the process name\n";
+        Cin>>process_id;
+        Cout<<"Enter Arrival time\n";
+        Cin>>arrival_time;
+        Cout<<"Enter the burst time required";
+        Cin>>arrival_time;
+	}
+}
+
+int main()
+{
+
+    Process process_list[4];
+
+}
 
