3c4eecfbf23022bbbb11c43e9e0365745f7d144f
Rupender-Bhati <bhati730@gmail.com>
2019-04-05 23:08:27 +0530

    Added Functions

--- Code.cpp
+++ Code.cpp
@@ -9,6 +9,8 @@ public:
     int time_waited;
 	int arrival_time;
 	int burst_time;
+	bool process_active;
+
 	string process_id;
 
 	Process()
@@ -20,7 +22,46 @@ public:
         cin>>arrival_time;
         cout<<"Enter the burst time required";
         cin>>arrival_time;
+        current_burst_time=burst_time;
+        time_waited=0;
 	}
+	int deduct_burstime(int x)
+	{
+	    int check=current_burst_time;
+
+	    if(check-x<=0)
+
+	    {
+	        current_burst_time=0;
+	        return (x-current_burst_time);
+	    }
+	    else
+
+            {current_burst_time=current_burst_time-x;
+                return 0;
+            }
+
+
+	}
+	void addtowait()
+	{
+	    if(process_active==false)
+        {
+            time_waited++;
+        }
+	}
+
+	void make_active()
+	{
+	    process_active=true;
+	}
+	void make_inactive()
+	{
+        process_active=false;
+	}
+
+
+
 };
 
 int main()
