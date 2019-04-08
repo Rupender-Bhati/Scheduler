ba69336916815f6e72c03c733b17f64c2ef4c6c9
Rupender-Bhati <bhati730@gmail.com>
2019-04-08 22:26:19 +0530

    Removed Errors

--- Code.c
+++ Code.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdbool.h>
 
 int clock=0;
 struct Process
@@ -10,59 +11,60 @@ struct Process
     int time_waited;
 	int arrival_time;
 	int burst_time;
-	bool process_active;
-    bool process_finished;
+	int process_active;
+	int process_finished;
 
 	char process_id[5];
 
 };
 
-void get_entries(struct Process &p)
+void get_entries(struct Process *p)
 {
     printf("Enter the process arrival time");
-    scanf("%d",p.arrival_time);
+    scanf("%d",&p->arrival_time);
     printf("\nEnter the process burst time");
-    scanf("%d",p.burst_time);
+    scanf("%d",&p->burst_time);
     printf("\nEnter the process name");
-    scanf("%s",p.process_id);
-    p.time_waited=0;
-    p.process_active=false;
-    p.current_burst_time=p.burst_time;
+    scanf("%s",&p->process_id);
+    p->time_waited=0;
+    p->process_active=false;
+    p->current_burst_time=p->burst_time;
+    p->process_active=0;
+    p->process_finished=0;
 
 }
 
-void make_active(struct Process &p)
+void make_active(struct Process *p)
 {
-    p.process_active=true;
+    p->process_active=1;
 }
-void make_inactive(struct Process &p)
+void make_inactive(struct Process *p)
 {
-    p.process_active=false;
+    p->process_active=0;
 }
-void finish_process(struct Process &p)
+void finish_process(struct Process *p)
 {
-    p.process_finished=true;
+    p->process_finished=1;
 
 }
-bool check_process_finish(struct Process p)
+int check_process_finish(struct Process p)
 {
-    if(p.process_finished==true)
-        return true;
+    if(p.process_finished==1)
+        return 1;
     else
-        return false;
-
+        return 0;
 }
-int deduct_burst_time(struct Process &p,int x)
+int deduct_burst_time(struct Process *p,int x)
 {
-    int check=p.current_burst_time;
+    int check=p->current_burst_time;
     if(check-x<=0)
     {
         finish_process(p);
-        p.current_burst_time=0;
+        (p->current_burst_time)=0;
         return(x-check);
     }
     else
-        p.current_burst_time-=x;
+        (p->current_burst_time)-=x;
         return 0;
 
 
@@ -76,7 +78,7 @@ int main()
     //Initialising processes.
     for(int i=0;i<4;i++)
     {
-        get_entries(process_list[i]);
+        get_entries(&(process_list[i]));
 
     }
 
