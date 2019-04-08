69e8b2a7e71f16d4e6066e45b8b1c22795c061ab
Rupender-Bhati <bhati730@gmail.com>
2019-04-08 21:19:43 +0530

    Changed to C lang

--- Code.cpp
+++ Code.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
 #include<string>
-using namespace std;
 
-class Process
+struct Process
 {
-public:
+
     int current_burst_time;
     int time_waited;
 	int arrival_time;
@@ -13,61 +13,6 @@ public:
 
 	string process_id;
 
-	Process()
-	{
-
-        cout<<"Enter the process name\n";
-        cin>>process_id;
-        cout<<"Enter Arrival time\n";
-        cin>>arrival_time;
-        cout<<"Enter the burst time required";
-        cin>>arrival_time;
-        current_burst_time=burst_time;
-        time_waited=0;
-	}
-	int deduct_burstime(int x)
-	{
-	    int check=current_burst_time;
-
-	    if(check-x<=0)
-
-	    {
-	        current_burst_time=0;
-	        return (x-current_burst_time);
-	    }
-	    else
-
-            {current_burst_time=current_burst_time-x;
-                return 0;
-            }
-
-
-	}
-	void addtowait()
-	{
-	    if(process_active==false)
-        {
-            time_waited++;
-        }
-	}
-
-	void make_active()
-	{
-	    process_active=true;
-	}
-	void make_inactive()
-	{
-        process_active=false;
-	}
-
-
-
 };
 
-int main()
-{
-
-    Process process_list[4];
-
-}
 
